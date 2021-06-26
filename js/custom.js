02a311bf549100ff76d234a78a09568b0fbcb997
WaterMenus <84546860+Watermenus@users.noreply.github.com>
2021-06-25 18:38:32 -0600

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,6 @@
 jQuery(document).ready(function( $ ) {
 
-  // Preloader
-  $(window).on('load', function() {
-    $('#preloader').delay(100).fadeOut('slow',function(){$(this).remove();});
-  });
+ 
 
   // Hero rotating texts
   $("#hero .rotating").Morphext({
